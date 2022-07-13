646b057365178543fa021e5ab71f8ae09535bf06
smallcaps <1827662823@qq.com>
2022-07-13 09:23:00 +0800

    初始化

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,17 @@ module.exports = {
   env: {
     node: true
   },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
+  extends: ['plugin:vue/essential', '@vue/standard'],
   parserOptions: {
     parser: '@babel/eslint-parser'
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    quotes: [1, 'single'],
+    semi: [1, 'never'],
+    'prettier/prettier': 'off',
+    'space-before-function-paren': 0,
+    'vue/multi-word-component-names': 0
   }
 }
